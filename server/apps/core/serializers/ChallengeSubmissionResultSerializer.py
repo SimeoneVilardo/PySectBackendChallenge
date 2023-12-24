6d5d31af96976fd18d0ce1bbf58ac0d1bbd4c08e
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-24 22:59:04 +0000

    Allow null in ChallengeSubmissionResultSerializer

--- server/apps/core/serializers/ChallengeSubmissionResultSerializer.py
+++ server/apps/core/serializers/ChallengeSubmissionResultSerializer.py
@@ -3,5 +3,5 @@ from server.apps.core.models.challenge_submission import ChallengeSubmission
 
 
 class ChallengeSubmissionResultSerializer(serializers.Serializer):
-    output = serializers.CharField(write_only=True, required=True, allow_null=False)
-    error = serializers.CharField(write_only=True, required=False, allow_null=True)
+    output = serializers.CharField(write_only=True, required=True, allow_null=True, allow_blank=True)
+    error = serializers.CharField(write_only=True, required=True, allow_null=True, allow_blank=True)
