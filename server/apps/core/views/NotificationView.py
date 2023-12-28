87e4409845d0f23f3998d03eec3737e865fc8472
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-28 11:35:49 +0000

    Add parsers

--- server/apps/core/views/NotificationView.py
+++ server/apps/core/views/NotificationView.py
@@ -1,9 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, parsers
+from rest_framework.parsers import BaseParser
+
+
+class PlainTextParser(BaseParser):
+    media_type = "text/plain"
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        return stream.read()
 
 
 class NotificationView(APIView):
+    parser_classes = [
+        parsers.JSONParser,
+        parsers.FormParser,
+        parsers.MultiPartParser,
+        PlainTextParser,
+    ]
+
     def post(self, request):
         print("POST request:")
         print("Headers:", request.headers)
