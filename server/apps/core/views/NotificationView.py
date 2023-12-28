fd5fd5361d9f1071dc7c7ac562145a298a4abacf
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-28 18:43:21 +0000

    create lambda by sns

--- server/apps/core/views/NotificationView.py
+++ server/apps/core/views/NotificationView.py
@@ -3,15 +3,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import BaseParser
 import json
+from server.apps.core.choices import ChallengeSubmissionStatusChoices
+
+from server.apps.core.models import ChallengeSubmission
+from server.apps.core.models.challenge import Challenge
+from server.apps.core.services.ChallengeSubmissionRunner import ChallengeSubmissionRunner
 
 
 class PlainTextParser(BaseParser):
     media_type = "text/plain"
 
     def parse(self, stream, media_type=None, parser_context=None):
-        stream_str = stream.read()
-        body = json.loads(stream_str)
-        return body
+        return stream.read()
 
 
 class NotificationView(APIView):
@@ -20,28 +23,42 @@ class NotificationView(APIView):
     ]
 
     def post(self, request):
-        body = request.body
-        print("POST request:")
-        print("Headers:", request.headers)
-        print("Body:", body)
-        print("Query parameters:", request.query_params)
-        try:
-            message = body.get("Message")
-            if not message:
-                raise Exception("Message not found in body")
-            message = json.loads(message)
-            challenge_submission_id = message.get("challenge_submission_id")
-            if not challenge_submission_id:
-                raise Exception("challenge_submission_id not found in message")
-            print("challenge_submission_id:", challenge_submission_id)
-        except Exception as e:
-            print("Exception:", e)
-
+        body = json.loads(request.body)
+        message = body.get("Message")
+        if not message:
+            raise Exception("Message not found in body")
+        message = json.loads(message)
+        challenge_submission_id = message.get("challenge_submission_id")
+        if not challenge_submission_id:
+            raise Exception("challenge_submission_id not found in message")
+        self.create_lambda_function(challenge_submission_id)
         return Response(status=status.HTTP_201_CREATED)
 
-    def get(self, request):
-        print("GET request:")
-        print("Headers:", request.headers)
-        print("Body:", request.data)
-        print("Query parameters:", request.query_params)
-        return Response(status=status.HTTP_200_OK)
+    def create_zip_file(self, challenge: Challenge, challenge_submission: ChallengeSubmission):
+        if challenge_submission.src_data is None:
+            raise Exception("No src_data found in challenge submission")
+        src_data = challenge_submission.src_data
+        with open("server/apps/core/lambda/lambda_function.py", "r") as file:
+            template = file.read()
+        src_data = template.replace("###SRC###", src_data)
+        input_file = challenge.input
+        zip_file = ChallengeSubmissionRunner.create_zip(input_file, src_data)
+        return zip_file
+
+    def init_challenge_submission(self, challenge_submission: ChallengeSubmission, function_name: str):
+        challenge_submission.lambda_name = function_name
+        challenge_submission.status = ChallengeSubmissionStatusChoices.READY
+        challenge_submission.save()
+
+    def create_lambda_function(self, challenge_submission_id: int):
+        challenge_submission = ChallengeSubmission.objects.select_related("challenge").get(id=challenge_submission_id)
+        challenge: Challenge = challenge_submission.challenge
+        try:
+            zip_file = self.create_zip_file(challenge, challenge_submission)
+            lambda_response = ChallengeSubmissionRunner.create_lambda_function(
+                f"submission_{challenge_submission.id}", zip_file
+            )
+            self.init_challenge_submission(challenge_submission, lambda_response["FunctionName"])
+        except Exception as e:
+            challenge_submission.status = ChallengeSubmissionStatusChoices.BROKEN
+            challenge_submission.save()
