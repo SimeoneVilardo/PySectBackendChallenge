cc7d5492716e57aa3673887915e21af98fc4d58f
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-28 23:06:05 +0000

    Use SNS in result view

--- server/apps/core/views/ChallengeSubmissionResultView.py
+++ server/apps/core/views/ChallengeSubmissionResultView.py
@@ -1,32 +1,32 @@
 import os
 from django.conf import settings
 from django.http import Http404
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from server.apps.core.choices import ChallengeSubmissionStatusChoices
+from server.apps.core.serializers import NotificationSerializer
 from server.apps.core.models import ChallengeSubmission, Challenge
 from server.apps.core.serializers import ChallengeSubmissionResultSerializer, ChallengeSubmissionSerializer
 from server.apps.core.services.ChallengeSubmissionRunner import ChallengeSubmissionRunner
 from server.apps.core.tasks.challenge_submission import check_submission_result
 
 
-class ChallengeSubmissionResultView(UpdateAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = ChallengeSubmissionResultSerializer
+class ChallengeSubmissionResultView(CreateAPIView):
+    serializer_class = NotificationSerializer
 
-    def partial_update(self, request, *args, **kwargs):
-        print("request.data", request.data)
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except serializers.ValidationError as e:
-            print("e.detail", e.detail)
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(status=status.HTTP_201_CREATED)
+
+    def perform_create(self, serializer):
         validated_data = serializer.validated_data
+        print("ChallengeSubmissionResultView.perform_create", validated_data)
 
         challenge_submission_id = validated_data["challenge_submission_id"]
         try:
