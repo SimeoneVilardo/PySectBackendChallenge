ac96cbd90eab5879d80da3c0867db461b9eefc3b
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-28 22:16:22 +0100

    Revert "Enable sns sub"
    This reverts commit cae5f0163f00d2f8566a6151d1e688a74a1c2678.

--- server/apps/core/views/ChallengeSubmissionResultView.py
+++ server/apps/core/views/ChallengeSubmissionResultView.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.http import Http404
 from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
-from rest_framework.parsers import BaseParser
 from rest_framework import status
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
@@ -15,21 +14,48 @@ from server.apps.core.services.ChallengeSubmissionRunner import ChallengeSubmiss
 from server.apps.core.tasks.challenge_submission import check_submission_result
 
 
-class PlainTextParser(BaseParser):
-    media_type = "text/plain"
-
-    def parse(self, stream, media_type=None, parser_context=None):
-        return stream.read()
-
-
 class ChallengeSubmissionResultView(UpdateAPIView):
-    parser_classes = [
-        PlainTextParser,
-    ]
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChallengeSubmissionResultSerializer
 
-    def post(self, request):
-        print("Headers:", request.headers)
-        print("Query parameters:", request.query_params)
-        print("Body:", request.body)
+    def partial_update(self, request, *args, **kwargs):
+        print("request.data", request.data)
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except serializers.ValidationError as e:
+            print("e.detail", e.detail)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        validated_data = serializer.validated_data
 
-        return Response(status=status.HTTP_201_CREATED)
+        challenge_submission_id = validated_data["challenge_submission_id"]
+        try:
+            challenge_submission = ChallengeSubmission.objects.select_related("challenge").get(
+                id=challenge_submission_id
+            )
+        except ChallengeSubmission.DoesNotExist:
+            raise Http404()
+        if challenge_submission.status != ChallengeSubmissionStatusChoices.RUNNING:
+            raise serializers.ValidationError({"error": "Challenge is not in RUNNING state"})
+
+        challenge_submission.error = validated_data["error"]
+        challenge_submission.output = (
+            validated_data["output"].strip().replace("\r\n", "\n").replace("\r", "\n")
+            if validated_data["output"]
+            else None
+        )
+
+        if challenge_submission.error:
+            challenge_submission.status = ChallengeSubmissionStatusChoices.FAILURE
+            challenge_submission.save()
+            return Response(status=status.HTTP_200_OK)
+
+        challenge: Challenge = challenge_submission.challenge
+        challenge_output = challenge.output.strip().replace("\r\n", "\n").replace("\r", "\n")
+        challenge_submission.status = (
+            ChallengeSubmissionStatusChoices.SUCCESS
+            if challenge_output == challenge_submission.output
+            else ChallengeSubmissionStatusChoices.FAILURE
+        )
+        challenge_submission.save()
+        return Response(status=status.HTTP_200_OK)
