bb5eec71e963a0ae0be5d431999f02923cb67239
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-28 18:31:53 +0000

    improve PlainTextParser

--- server/apps/core/views/NotificationView.py
+++ server/apps/core/views/NotificationView.py
@@ -9,7 +9,9 @@ class PlainTextParser(BaseParser):
     media_type = "text/plain"
 
     def parse(self, stream, media_type=None, parser_context=None):
-        return stream.read()
+        stream_str = stream.read()
+        body = json.loads(stream_str)
+        return body
 
 
 class NotificationView(APIView):
@@ -18,20 +20,15 @@ class NotificationView(APIView):
     ]
 
     def post(self, request):
-        print("Headers:", request.headers)
-        print("Query parameters:", request.query_params)
-        try:
-            body = json.loads(request.body)
-            message = body.get("Message")
-            if not message:
-                raise Exception("Message not found in body")
-            message = json.loads(message)
-            challenge_submission_id = message.get("challenge_submission_id")
-            if not challenge_submission_id:
-                raise Exception("challenge_submission_id not found in message")
-            print("challenge_submission_id:", challenge_submission_id)
-        except Exception as e:
-            print("Error:", str(e))
+        body = request.body
+        message = body.get("Message")
+        if not message:
+            raise Exception("Message not found in body")
+        message = json.loads(message)
+        challenge_submission_id = message.get("challenge_submission_id")
+        if not challenge_submission_id:
+            raise Exception("challenge_submission_id not found in message")
+        print("challenge_submission_id:", challenge_submission_id)
 
         return Response(status=status.HTTP_201_CREATED)
 
