3c7cc8a976ee31badf0cada113a4ba3b03892e06
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-29 00:00:52 +0000

    Revert "sns sub"
    This reverts commit 2639a3beee7a6385be0122b725d244132b8b598b.

--- server/apps/core/views/ChallengeSubmissionInitView.py
+++ server/apps/core/views/ChallengeSubmissionInitView.py
@@ -14,20 +14,48 @@ from server.apps.core.serializers import NotificationSerializer
 from server.apps.core.services.ChallengeSubmissionRunner import ChallengeSubmissionRunner
 
 
-class PlainTextParser(BaseParser):
-    media_type = "text/plain"
+class ChallengeSubmissionInitView(generics.CreateAPIView):
+    serializer_class = NotificationSerializer
 
-    def parse(self, stream, media_type=None, parser_context=None):
-        return stream.read()
-
-
-class ChallengeSubmissionInitView(APIView):
-    parser_classes = [
-        PlainTextParser,
-    ]
-
-    def post(self, request):
-        print("Headers:", request.headers)
-        print("Query parameters:", request.query_params)
-        print("Body:", request.body)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
         return Response(status=status.HTTP_201_CREATED)
+
+    def perform_create(self, serializer):
+        print("validated_data", serializer.validated_data)
+        message = serializer.validated_data.get("Message")
+        challenge_submission_id = message.get("challenge_submission_id")
+        if not challenge_submission_id:
+            raise Exception("challenge_submission_id not found in message")
+        self.create_lambda_function(challenge_submission_id)
+
+    def create_zip_file(self, challenge: Challenge, challenge_submission: ChallengeSubmission):
+        if challenge_submission.src_data is None:
+            raise Exception("No src_data found in challenge submission")
+        src_data = challenge_submission.src_data
+        with open("server/apps/core/lambda/lambda_function.py", "r") as file:
+            template = file.read()
+        src_data = template.replace("###SRC###", src_data)
+        input_file = challenge.input
+        zip_file = ChallengeSubmissionRunner.create_zip(input_file, src_data)
+        return zip_file
+
+    def init_challenge_submission(self, challenge_submission: ChallengeSubmission, function_name: str):
+        challenge_submission.lambda_name = function_name
+        challenge_submission.status = ChallengeSubmissionStatusChoices.READY
+        challenge_submission.save()
+
+    def create_lambda_function(self, challenge_submission_id: int):
+        challenge_submission = ChallengeSubmission.objects.select_related("challenge").get(id=challenge_submission_id)
+        challenge: Challenge = challenge_submission.challenge
+        try:
+            zip_file = self.create_zip_file(challenge, challenge_submission)
+            lambda_response = ChallengeSubmissionRunner.create_lambda_function(
+                f"submission_{challenge_submission.id}", zip_file
+            )
+            self.init_challenge_submission(challenge_submission, lambda_response["FunctionName"])
+        except Exception as e:
+            challenge_submission.status = ChallengeSubmissionStatusChoices.BROKEN
+            challenge_submission.save()
