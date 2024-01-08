bd6c10c06bc18ffb0cb5dddd577e576c81164acf
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-01-08 23:25:49 +0000

    More logs

--- server/settings/components/logging.py
+++ server/settings/components/logging.py
@@ -1,22 +1,46 @@
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {message}",
+            "style": "{",
+        },
+    },
     "handlers": {
-        "file": {
+        "file_error": {
             "level": "WARNING",
             "class": "logging.handlers.TimedRotatingFileHandler",
-            "filename": "/var/www/log/pysect-backend.log",
+            "filename": "/var/www/log/pysect-backend-error.log",
             "when": "midnight",
             "interval": 1,
             "backupCount": 365,
+            "formatter": "verbose",
         },
         "console": {
             "level": "INFO",
             "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+        "server": {
+            "level": "INFO",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": "/var/www/log/pysect-backend-access.log",
+            "when": "midnight",
+            "interval": 1,
+            "backupCount": 365,
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["server"],
+            "level": "INFO",
+            "propagate": False,
         },
     },
     "root": {
-        "handlers": ["file", "console"],
+        "handlers": ["file_error", "console"],
         "level": "INFO",
     },
 }
