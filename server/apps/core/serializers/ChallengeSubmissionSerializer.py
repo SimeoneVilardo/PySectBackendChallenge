e300fa3b69be50186b8fb30ff46f03d97481f1ca
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-01-14 01:46:13 +0000

    add fields to serializer

--- server/apps/core/serializers/ChallengeSubmissionSerializer.py
+++ server/apps/core/serializers/ChallengeSubmissionSerializer.py
@@ -5,3 +5,4 @@ from server.apps.core.models.challenge_submission import ChallengeSubmission
 class ChallengeSubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChallengeSubmission
+        fields = "__all__"
