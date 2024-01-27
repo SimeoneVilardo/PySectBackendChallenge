eadf7ea7c81eeb319e4e254e6e2c5f62abe518fa
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-01-27 04:43:00 +0100

    Change challenge points to decimal

--- server/apps/core/models/challenge.py
+++ server/apps/core/models/challenge.py
@@ -1,11 +1,16 @@
 from django.db import models
+from decimal import Decimal
 
 
 class Challenge(models.Model):
     name = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
     description = models.TextField()
-    points = models.IntegerField()
+    points = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        choices=[(Decimal(x), str(x)) for x in [i * 0.5 for i in range(11)]],
+    )
     input_sample = models.TextField()
     output_sample = models.TextField()
     memory_limit = models.IntegerField()
